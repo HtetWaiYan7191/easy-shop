fce5393b2ec961b23b0b412274f5273e45c90105
HtetWaiYan7191 <htetwaiyan7191@gmail.com>
2023-07-12 22:44:46 +0630

    Implement require components

---
+++ easy-shop/src/components/ProductsContainer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ProductsContainer = () => {
+  return (
+    <div>
+      <h1>This is ProductsContainer</h1>
+    </div>
+  )
+}
+
+export default ProductsContainer
