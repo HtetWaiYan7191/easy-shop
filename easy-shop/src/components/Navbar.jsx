9690e3001c89b69fa63579a98d04d160964af203
HtetWaiYan7191 <htetwaiyan7191@gmail.com>
2023-07-18 20:31:38 +0630

    Finish basic navbar style and structure

--- easy-shop/src/components/Navbar.jsx
+++ easy-shop/src/components/Navbar.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
 
 const Navbar = () => {
   return (
-    <div>
-      
-    </div>
+    <nav className='flex justify-between items-center border border-b-1'>
+      <h1 className='text-3xl font-semibold ps-10 py-5'>Easy Shop</h1>
+      <ul className='flex '>
+        <li className='me-5'><NavLink to="/">Products</NavLink></li>
+        <li className='me-5'><NavLink to="/favorite">Favorite</NavLink></li>
+        <li className='me-10'><NavLink to="/cartItems">CartItems</NavLink></li>
+      </ul>
+    </nav>
   )
 }
 
