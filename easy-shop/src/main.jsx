a45f148176ce68b7c78434fbc0c84a47f3dc217e
HtetWaiYan7191 <htetwaiyan7191@gmail.com>
2023-07-18 20:31:57 +0630

    wrap provider in main.jsx

--- easy-shop/src/main.jsx
+++ easy-shop/src/main.jsx
@@ -3,9 +3,15 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
+import { Provider } from 'react-redux';
+import store from './redux/store';
+import Navbar from "./components/Navbar.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter>
-    <App />
+  <Navbar/>
+  <Provider store={store}>
+      <App />
+  </Provider>
   </BrowserRouter>
 );
