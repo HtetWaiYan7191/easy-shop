12d7468954916b9a429d304b5f6b7110bca3b24b
HtetWaiYan7191 <htetwaiyan7191@gmail.com>
2023-07-18 23:34:17 +0630

    change reaction position

--- easy-shop/src/components/Products.jsx
+++ easy-shop/src/components/Products.jsx
@@ -27,14 +27,14 @@ const Products = ({product}) => {
 
   return (
     <div className='product-card border  border-1 relative'>
-      <button className={fav ? 'absolute right-2 top-4 text-red-500' : 'absolute right-2 top-4 '} type='button' onClick={handleClick}><AiFillHeart/></button>
+      <button className={fav ? 'absolute right-3 bottom-32 text-red-500' : 'absolute right-3 bottom-32 '} type='button' onClick={handleClick}><AiFillHeart/></button>
       <figure className='flex justify-center my-5'>
         <img src={product.image}   alt="product-image"  className='product-image'/>
       </figure>
       <h2 className='text-md font-semibold text-center max-h-[24px] overflow-auto'>{product.title}</h2>
       <p className=' text-gray-500 product-description px-5 my-4 '>{product.description}</p>
-      <div className='text-center py-4 text-slate-600'>${product.price}</div>
-      <button className= {!inCart ? ' w-full p-3 bg-teal-400 border hover:bg-teal-300 active:bg-teal-500' : 'w-full p-3 bg-red-500 border hover:bg-red-400 active:bg-red-600 text-white'} onClick={handleCart}>{!inCart ? 'Add to Cart' : 'Remove From Cart'}</button>
+      <div className='text-center py-4 text-teal-500'>${product.price}</div>
+      <button className= {!inCart ? ' w-full p-3 bg-teal-400 border hover:bg-teal-300 active:bg-teal-500' : 'w-full p-3 bg-red-600 border hover:bg-red-500 active:bg-red-500 text-white'} onClick={handleCart}>{!inCart ? 'Add to Cart' : 'Remove From Cart'}</button>
       <button className=' w-full p-3 bg-sky-400 border hover:bg-sky-300 active:bg-sky-500'>Details</button>
     </div>
   )
