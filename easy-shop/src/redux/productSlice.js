1aec47e5e600df463271d512f4c784155eaa98c8
HtetWaiYan7191 <htetwaiyan7191@gmail.com>
2023-07-18 23:02:06 +0630

    Add actions to handle favorite

--- easy-shop/src/redux/productSlice.js
+++ easy-shop/src/redux/productSlice.js
@@ -3,22 +3,44 @@ import axios from 'axios';
 
 const initialState = {
     loading : false,
-    favorite : false, 
-    cart: false, 
     value: [],
     error : '',
 };
 const URL = 'https://fakestoreapi.com/products';
 export const fetchProducts = createAsyncThunk('product/fetchProducts', async () => {
     const result = await axios.get(URL);
-   console.log(result.data)
-   return result.data;
+    const products = result.data.map((product) => ({
+        id: product.id,
+        title: product.title,
+        category: product.category,
+        description: product.description,
+        image: product.image,
+        price: product.price,
+        favorite: false,
+        cart: false,
+    }));
+   return products;
 })
 
 const productSlice = createSlice({
     name: 'products',
     initialState,
-    reducers:{},
+    reducers:{
+        addFavoriteList: (state,action) => {
+            const newState = state.value.map((product) => {
+                if(product.id !== action.payload) return product;
+                return ({...product, favorite:true})
+            });
+            state.value = newState;
+        },
+        removeFavoriteList: (state, action) => {
+            const newState = state.value.map((product) => {
+                if(product.id !== action.payload) return product;
+                return ({...product, favorite: false})
+            });
+            state.value = newState;
+        }
+    },
     extraReducers: (builder) => {
         builder.addCase(fetchProducts.pending, (state) => {
             state.loading = true;
@@ -36,3 +58,4 @@ const productSlice = createSlice({
     }
 })
 export default productSlice.reducer;
+export const {addFavoriteList, removeFavoriteList} = productSlice.actions;
