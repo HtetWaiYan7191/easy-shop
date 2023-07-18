5b3db4cee8141d4b472ac95490fccd168c752278
HtetWaiYan7191 <htetwaiyan7191@gmail.com>
2023-07-18 23:14:11 +0630

    Add structure for FavoriteItem

--- easy-shop/src/components/Navbar.jsx
+++ easy-shop/src/components/Navbar.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
-import { NavLink } from 'react-router-dom'
+import { NavLink } from 'react-router-dom';
+import '../styles/Navbar.css';
 
 const Navbar = () => {
   return (
--- easy-shop/src/pages/FavoriteItems.jsx
+++ easy-shop/src/pages/FavoriteItems.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
+import Products from '../components/Products';
 
 const FavoriteItems = () => {
+  const originalFavItems = useSelector((state) => state.products.value);
+  const favItems = originalFavItems.filter((item) => item.favorite === true);
+  console.log(favItems)
   return (
-    <div>
-      This is favorite
+    <div className='product-container grid grid-cols-4 gap-10 my-10 w-[80%] mx-auto'>
+      {favItems?.map((favItem) => (
+        <Products key={favItem.id} product={favItem}/>
+))}
     </div>
   )
 }
