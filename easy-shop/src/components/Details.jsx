87d05de71c3a8de604c195c6243a87c1444dcff3
HtetWaiYan7191 <htetwaiyan7191@gmail.com>
2023-07-19 21:29:48 +0630

    Finish Detail page

--- easy-shop/src/components/Details.jsx
+++ easy-shop/src/components/Details.jsx
@@ -1,11 +1,41 @@
-import React from 'react'
-import { useParams } from 'react-router-dom'
+import React, { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux';
+import { useParams } from 'react-router-dom';
+import { fetchProductData } from '../redux/detailSlice';
+import {AiFillStar} from 'react-icons/ai';
 
 const Details = () => {
-  const { product } = useParams();
+  const { productId } = useParams();
+  const dispatch = useDispatch();
+  const product = useSelector((state) =>state.detail.value);
+  console.log(product)
+
+  useEffect(() => {
+    dispatch(fetchProductData(productId))
+  }, [dispatch])
   return (
-    <div>
-      This is Details Page {product}
+    <div className='detail-container flex items-center h-screen'>
+      <div className='detail-card flex w-[80%] mx-auto items-center'>
+          <figure className=' me-10 border-r-2 pe-5 border-r-slate-500 w-[500px] h-[500px] flex items-center'>
+            <img src={product.image}  className=" object-contain" alt="" />
+          </figure>
+          <div className='product-details-text'>
+              <h2 className='text-xl font-semibold'>{product.title}</h2>
+              <div className="rating-container flex my-5 w-[20%] justify-between items-center">
+                <div className="rating-icon-container flex">
+                  <AiFillStar className='text-yellow-500'/>
+                  <AiFillStar className='text-yellow-500'/>
+                  <AiFillStar className='text-yellow-500'/>
+                  <AiFillStar className='text-yellow-500'/>
+                  <AiFillStar/>
+                </div>
+                <span className='text-blue-500'>{product.rating.count} ratings</span>
+              </div>
+              <p className=' text-justify text-gray-800 my-5 '>{product.description}</p>
+              <div className='text-lg mb-3 '>Price : <span className='text-2xl text-red-500'>${product.price}</span></div>
+              <div>Category: <span className='text-md font-semibold'>{product.category}</span></div>
+          </div>
+      </div>
     </div>
   )
 }
