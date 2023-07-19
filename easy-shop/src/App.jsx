64fb9ab38e37b09915dd39b9be7b959b4b491fe6
HtetWaiYan7191 <htetwaiyan7191@gmail.com>
2023-07-19 13:18:31 +0630

    Update Details component

--- easy-shop/src/App.jsx
+++ easy-shop/src/App.jsx
@@ -3,6 +3,7 @@ import { Routes, Route } from 'react-router-dom';
 import ProductsContainer from './pages/ProductsContainer';
 import FavoriteItems from './pages/FavoriteItems';
 import CartItems from './pages/CartItems';
+import Details from './components/Details';
 
 
 const App = () => {
@@ -11,7 +12,7 @@ const App = () => {
       <Route path='/' element={<ProductsContainer/>}></Route>
       <Route path='/favorite' element={<FavoriteItems/>}></Route>
       <Route path='/cartItems' element={<CartItems/>}></Route>
-      <Route path='/details'></Route>
+      <Route path='/details/:product' element={<Details/>}></Route>
     </Routes>
   )
 }
--- easy-shop/src/components/Details.jsx
+++ easy-shop/src/components/Details.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import { useParams } from 'react-router-dom'
 
 const Details = () => {
+  const { product } = useParams();
   return (
     <div>
-      
+      This is Details Page {product}
     </div>
   )
 }
